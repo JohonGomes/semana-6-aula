5b9b957905b85ac9a5f5c233274279321e96833e
johon.gomes <johon.gomes@hotmail.com>
2024-10-08 21:10:10 -0300

    adição do subtotal na listagem de produtos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 // Lista de produtos disponíveis com nome e preço
 const produtosDisponiveis = [
   { nome: "Camisa", preco: 50.0 },
@@ -10,3 +8,67 @@ const produtosDisponiveis = [
 
 // Carrinho de compras como um array de objetos
 let carrinho = [];
+
+// Função para capturar produtos e adicionar ao carrinho
+function capturarProdutos() {
+  let continuarComprando = true;
+
+  while (continuarComprando) {
+    const nomeProduto = prompt("Digite o nome do produto: \n Camisa, Calça, Sapato, Boné");
+    const produtoEncontrado = produtosDisponiveis.find(
+      (produto) => produto.nome.toLowerCase() === nomeProduto.toLowerCase()
+    );
+
+    if (produtoEncontrado) {
+      const quantidade = parseInt(
+        prompt(`Quantidade de ${produtoEncontrado.nome}`)
+      );
+      carrinho.push({
+        nome: produtoEncontrado.nome,
+        preco: produtoEncontrado.preco,
+        quantidade: quantidade,
+      });
+      alert(`Produto adicionado: ${produtoEncontrado.nome}, Preço: R$${produtoEncontrado.preco}, Quantidade: ${quantidade}`);
+    } else {
+      alert("Produto não encontrado!");
+    }
+
+    continuarComprando = confirm("Deseja adicionar mais produtos ao carrinho?");
+  }
+
+  calcularTotal();
+}
+
+// Função para calcular o total do carrinho
+function calcularTotal() {
+  const total = carrinho.reduce(
+    (soma, item) => soma + item.preco * item.quantidade,
+    0
+  );
+
+  alert(`Total do carrinho: R$${total.toFixed(2)}`);
+}
+
+// Função para exibir o carrinho
+function exibirCarrinho() {
+  if (carrinho.length === 0) {
+    alert("O carrinho está vazio!");
+  } else {
+    let mensagem = "Produtos no carrinho:\n\n";
+    let valorTotal = 0;
+
+    carrinho.forEach((item) => {
+    const subTotal = item.preco * item.quantidade;
+    valorTotal += subTotal;
+    mensagem += `${item.nome} - Preço: R$${item.preco} - Quantidade: ${item.quantidade} - SubTotal: R$${subTotal.toFixed(2)}\n`;
+    });
+
+    alert(mensagem);
+  }
+}
+
+// Exemplo de uso
+capturarProdutos();
+exibirCarrinho();
+
+// Lógica para calcular o subtotal e exibir com alert()
